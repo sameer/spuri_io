900839f4cea338d01e1201cc10895b15286b96cb
Sameer <spuri4096@gmail.com>
2018-07-22 00:24:23 -0700

    Use better variable names for build.rs

--- build.rs
+++ build.rs
@@ -7,14 +7,15 @@ use std::io::prelude::*;
 
 fn main() {
     askama::rerun_if_templates_changed();
-    let mut f = File::open("files/style.css").unwrap();
+
+    let mut css_file = File::open("files/style.css").unwrap();
     let mut hasher = openssl::sha::Sha512::new();
-    let mut buf = vec![];
-    f.read_to_end(&mut buf).unwrap();
-    hasher.update(&buf);
+    let mut css_file_contents = vec![];
+    css_file.read_to_end(&mut css_file_contents).unwrap();
+    hasher.update(&css_file_contents);
     let hash: &[u8] = &hasher.finish();
     let hash = format!("sha512-{}", base64::encode(hash));
-    let mut f = File::create("src/css_file_hash").unwrap();
-    f.write_all(hash.as_bytes()).unwrap();
-    f.sync_all().unwrap();
+    let mut css_file_hash = File::create("src/css_file_hash").unwrap();
+    css_file_hash.write_all(hash.as_bytes()).unwrap();
+    css_file_hash.sync_all().unwrap();
 }
