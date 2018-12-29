a42dba9fa605dacd66f656037a72337ea80c3073
Sameer <11097096+sameer@users.noreply.github.com>
2018-12-29 12:22:55 -0500

    Update openssl

--- test/build_openssl.sh
+++ test/build_openssl.sh
@@ -9,9 +9,9 @@ rm -r ${OPENSSL_DIR}
 mkdir /tmp/build/
 [[ -d /tmp/build ]] || die "Failed to create build directory"
 cd /tmp/build/
-curl https://www.openssl.org/source/openssl-1.1.0h.tar.gz -o /tmp/build/openssl.tar.gz 
+curl https://www.openssl.org/source/openssl-1.1.1a.tar.gz -o /tmp/build/openssl.tar.gz
 [[ -f /tmp/build/openssl.tar.gz ]] || die "Failed to download openssl source code"
-tar xzf /tmp/build/openssl.tar.gz && cd openssl-1.1.0h
+tar xzf /tmp/build/openssl.tar.gz && cd openssl-1.1.1a
 ./Configure --prefix=${OPENSSL_DIR} --openssldir=${OPENSSL_DIR} linux-x86_64 -fPIC -g no-shared
 make -j$(nproc)
 make install
